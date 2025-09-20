e58a53d797878e7fda9fb0b6f6f08599d9c3d185
Alekseeva-Dana <s02240056@gse.cs.msu.ru>
2025-09-20 19:40:52 +0300

    Задача 2.27

---
+++ homework/227.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+unsigned int replace_bits(unsigned int x, unsigned int y, int p,int n) {
+
+	unsigned int inverted_y = ~y;
+
+	unsigned int mask_y = (~0u) << (sizeof(unsigned int)*8 - n);
+	unsigned int high_bits = inverted_y & mask_y;
+
+	high_bits = high_bits >> (sizeof(unsigned int)*8 - n - p);
+
+	unsigned int mask_clear = (~0u) << (sizeof(unsigned int)*8 - n);
+	mask_clear = ~(mask_clear >>(sizeof(unsigned int)*8 - n - p));
+
+	x = x & mask_clear;
+
+	x = x | high_bits;
+
+	return x;
+}
+
+int main() {
+	unsigned int x, y;
+	int p, n;
+	printf("Введите x, y, p, n: ");
+	scanf("%x %x %d %d", &x, &y, &p, &n);
+	
+	unsigned int res = replace_bits(x, y, p, n);
+	printf("Результат: %u", res);
+
+	return 0;
+}
